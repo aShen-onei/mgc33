4ff1a250441ef1ffc7748e8736480266bf674ee6
aShen_onei <996052370@qq.com>
2019-08-04 16:00:03 +0800

    解决

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,8 @@
 import HomePage from './pages/home.vue';
-<<<<<<< Updated upstream
-import APMPAGE from './pages/apm';
 import KONGTIAOPAGE from './pages/kongtiao';
-import LIGHTPAGE from './pages/light'
-=======
+import LIGHTPAGE from './pages/light';
 import APMPAGE from './pages/apm.vue';
 import CURTAINPAGE from './pages/curtain.vue'
->>>>>>> Stashed changes
 export default [
   {
     path: '/',
@@ -17,16 +13,15 @@ export default [
     component: APMPAGE
   },
   {
-<<<<<<< Updated upstream
     path:'/kongtiao/',
     component:KONGTIAOPAGE
   },
   {
     path:'/light/',
     component:LIGHTPAGE
-=======
+    },
+    {
     path:'/curtain/',
     component:CURTAINPAGE
->>>>>>> Stashed changes
-  }
+    }
 ];
